de984498053b8df074058890f1c2f12c32cd2950
Micah Lawson <micahlawson@protonmail.com>
2025-01-18 13:41:27 -0500

    Added initial player with movement and walk cycle

---
+++ scripts/player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+@export var playerAnimation: AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	
+	var direction := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	
+	if direction:
+		# Sprite Orientation
+		if direction.x > 0:
+			playerAnimation.flip_h = false
+		elif direction.x < 0:
+			playerAnimation.flip_h = true
+		
+		playerAnimation.play("move")
+		
+		velocity = direction * SPEED
+	else:
+		playerAnimation.play("idle")
+		
+		velocity = Vector2.ZERO
+	
+	move_and_slide()
