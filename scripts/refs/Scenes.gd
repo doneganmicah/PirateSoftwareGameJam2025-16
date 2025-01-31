afa728a99aa149b7a72125877a38fc4ab4c808ba
Micah Lawson <micahlawson@protonmail.com>
2025-01-31 01:21:55 -0500

    Add main menu

--- scripts/refs/Scenes.gd
+++ scripts/refs/Scenes.gd
@@ -1,6 +1,7 @@
 extends Node
 class_name Scenes
 
+const main_menu : String = "res://scenes/main_menu.tscn"
 const level_one : String = "res://scenes/levels/Level_One.tscn"
 const test_level : String = "res://scenes/levels/tests/testing_level.tscn"
 
